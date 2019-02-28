de2a45f6b19dae7b43e28227207c8070de5120c8
Kranmas <njkranmas@labs.neit.edu>
2019-02-28 14:58:38 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+var io = require('socket.io')(process.env.PORT||5000);
+var shortid = require('shortid');
+
+var players = [];
+//var playerCount = 0;
+
+console.log("Server Running");
+
+io.on('connection', function(socket){
+    console.log("Connected to Unity"); 
+    var thisPlayerId = shortid.generate();
+
+    var player = {
+        id:thisPlayerId,
+        position:{
+            v:0
+            
+        }
+    }
+    
+    players[thisPlayerId] = player;
+
+    socket.emit("register", {id:thisPlayerId});
+    socket.broadcast.emit("spawn", players[playerId]);
+    //console.log("Sending spawn to new with ID ", thisPlayerId);
+
+    for(var playerId in players){
+        if(playerId == thisPlayerId)
+        continue;
+        socket.emit('spawn', players[playerId]);
+
+    }
+
+    socket.on('sayhello', function(data){
+        console.log("Unity Game says hello");
+        socket.emit('talkback');
+    });
+
+    socket.on('disconnect', function(){
+        console.log("Player Disconnected");
+        //playerCount--;
+        delete players[thisPlayerId];
+        socket.broadcast.emit('disconnected', {id:thisPlayerId})
+    });
+
+    socket.on('move', function(data){
+        data.id = thisPlayerId;
+        console.log("Player Moved", JSON.stringify(data));
+        socket.broadcast.emit('move', data);
+    });
+
+    socket.on('updatePostiton', function(data){
+        data.id = thisPlayerId;
+        socket.broadcast.emit('updatePosition', data);
+    });
+});
